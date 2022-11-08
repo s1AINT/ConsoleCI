5f73ecb406d0eb216e3a6e8f49605fc0c88b2db5
HP <HP@VASUOK>
2022-11-08 23:00:05 +0200

    Добавьте файлы проекта.

---
+++ ConsoleCI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
